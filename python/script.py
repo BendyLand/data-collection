b07b4bc19ca34256b030f07dc9058399457ca1b5
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-02-29 10:28:49 -0500

    Begin gathering weather data

--- python/script.py
+++ python/script.py
@@ -1,19 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
-""" 
-Guide:
-https://realpython.com/beautiful-soup-web-scraper-python/#what-is-web-scraping
-
-Website with sample data to practice scraping:
-https://realpython.github.io/fake-jobs/ 
-
+"""
+Idea:
+Scrape weather data from various sources and display them in a single area,
+along with combination info (average, median, etc.)
 """
 
-URL = "https://realpython.github.io/fake-jobs/"
+URL = "https://weather.com/weather/tenday/l/Salem+CT?canonicalCityId=d6b4f8cb5f0e1db1761b2933184fd1de0779ec1fb8e9352bba2f7526d0000677"
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
 
-# print(page.text)
-# print(soup.prettify())
+parent = soup.find(attrs={"class": "Card--content--1GQMr DailyForecast--CardContent--2YlvT"})
+spans = parent.find_all("span")
+temps = []
+for span in spans:
+    if "lowTempValue" in str(span):
+        if span.text[0] != "/":
+            temps.append(int(span.text[:-1]))
 
+print("Average temp for the next two weeks:", str(sum(temps) / len(temps)))
