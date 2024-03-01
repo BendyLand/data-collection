e76a5582ec9e2d2721166baf030ffa4954ba3cb0
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-02-29 21:33:52 -0500

    Start implementing second website scraper

--- python/script.py
+++ python/script.py
@@ -8,23 +8,40 @@ along with combination info (average, median, etc.)
 """
 
 url_1 = "https://weather.com/weather/tenday/l/Salem+CT?canonicalCityId=d6b4f8cb5f0e1db1761b2933184fd1de0779ec1fb8e9352bba2f7526d0000677"
+url_2 = "https://www.weatherwx.com/14dayweather/ct/east+windsor.html"
 
 
 class WebScraper:
     def __init__(self):
-        self.urls = [url_1]
+        self.urls = [url_1, url_2]
+
+    def parse_website(self, url):
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, "html.parser")
+        return soup
 
     def scrape(self, url):
         if url == self.urls[0]:
             Website1Scraper().scrape(url)
+        elif url == self.urls[1]:
+            Website2Scraper().scrape(url)
         else:
             print("Invalid URL")
 
 
-class Website1Scraper:
+class Website2Scraper:
     def scrape(self, url):
-        page = requests.get(url)
-        soup = BeautifulSoup(page.content, "html.parser")
+        soup = WebScraper().parse_website(url)
+        # gets the table of values
+        forecast = soup.find(attrs={"id": "elbat"})
+        # prints it sub-table at a time
+        for line in forecast:
+            print(line)
+
+
+class Website1Scraper(WebScraper):
+    def scrape(self, url):
+        soup = WebScraper().parse_website(url)
         try:
             parent = soup.find(
                 attrs={
@@ -37,13 +54,15 @@ class Website1Scraper:
                 if "lowTempValue" in str(span):
                     if span.text[0] != "/":
                         temps.append(int(span.text[:-1]))
-            print(
-                "Average low temp for the next two weeks:", str(sum(temps) / len(temps))
-            )
+
+            return temps
         except:
             print("Unable to extract temperatures")
 
 
 scraper = WebScraper()
 url1 = scraper.urls[0]
-scraper.scrape(url1)
+temps1 = scraper.scrape(url1)
+
+url2 = scraper.urls[1]
+scraper.scrape(url2)
