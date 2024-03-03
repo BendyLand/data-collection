71480f41567844eb9a22bbd6fa1614d758f51189
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-03-03 16:35:59 -0500

    Update Scraper method name

--- scala/src/main/scala/Main.scala
+++ scala/src/main/scala/Main.scala
@@ -9,5 +9,5 @@ import bland.scraper.*
 @main def run() = 
     val url = "https://weather.com/weather/tenday/l/Salem+CT?canonicalCityId=d6b4f8cb5f0e1db1761b2933184fd1de0779ec1fb8e9352bba2f7526d0000677"
     val scraper = Scraper
-    val result = scraper.parseWebsite(url)
+    val result = scraper.scrape(url)
     println(result)
--- scala/src/main/scala/Scraper.scala
+++ scala/src/main/scala/Scraper.scala
@@ -5,5 +5,5 @@ import org.jsoup.nodes.Document
 import scala.jdk.CollectionConverters.*
 
 object Scraper:
-    def parseWebsite(url: String): Document = 
+    def scrape(url: String): Document = 
         Jsoup.connect(url).get()
