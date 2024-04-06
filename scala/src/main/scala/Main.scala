adc284895eddc75ef19051a40549d7ae916377f4
BendyLand <128928536+BendyLand@users.noreply.github.com>
2024-04-05 21:15:01 -0400

    Begin data collection project in scala

--- scala/src/main/scala/Main.scala
+++ scala/src/main/scala/Main.scala
@@ -1,8 +1,22 @@
-/*  
-! Idea: 
-! Log file analysis
-? The log files will be provided by another program, yet to be implemented.
-*/
+import scala.io.*
 
-@main def run() = 
-    println("Hi")
+enum LogType:
+    case Request, LogMessage
+@main def run() =
+    val file = readFile()
+    val lines = file.split('\n')
+    val logTypes = lines.map(line => classifyLogType(line))
+    val pairs = lines.zip(logTypes)
+    pairs.foreach(println)
+
+
+def classifyLogType(log: String): LogType = 
+    if log.contains("HTTP/1.1") then
+        LogType.Request
+    else
+        LogType.LogMessage
+
+
+def readFile(): String = 
+    val file = Source.fromFile("sample_logs.txt")
+    try file.mkString finally file.close()
